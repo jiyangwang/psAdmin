761a1ca6c45b50abe5e93100ffcd591157ce9ae9
Jiyang Wang <jiyang.wang.us@gmail.com>
2016-06-16 15:38:09 -0700

    Flux

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -3,20 +3,28 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = require('react-router').Link;
-var AuthorApi = require('../../api/authorApi');
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
 var AuthorList = require('./authorList');
 
 var AuthorPage = React.createClass({
 	getInitialState: function() {
 		return {
-			authors: []
+			authors: AuthorStore.getAllAuthors()
 		};
 	},
+	
+	componentWillMount: function() {
+		AuthorStore.addChangeListener(this._onChange);
+	},
 
-	componentDidMount: function() {
-		if (this.isMounted()) {
-			this.setState({ authors: AuthorApi.getAllAuthors() });
-		}
+	//Clean up when this component is unmounted
+	componentWillUnmount: function() {
+		AuthorStore.removeChangeListener(this._onChange);
+	},
+
+	_onChange: function() {
+		this.setState({ authors: AuthorStore.getAllAuthors() });
 	},
 
 	render: function() {
